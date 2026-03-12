de97774d78fc350add761458901555a89ff34207
imanchalsingh <singhanchal159357@gmail.com>
2026-03-12 11:11:14 +0530

    added route of front page

--- user-frontend/src/App.tsx
+++ user-frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Register from './components/Register'
 import AlumniDashboard from './pages/AlumniDashboard'
+import FrontPage from './components/FrontPage'
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Register />} />
+      <Route path="/" element={<FrontPage />} />
+      <Route path='/register' element={<Register/>}/>
       <Route path="/alumni-dashboard" element={<AlumniDashboard />} />
     </Routes>
   )
